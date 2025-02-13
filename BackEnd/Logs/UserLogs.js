9d5c26e4cc6c05a34a08923e06c24584ec17c3ba
UMBFB1 <umbreongame0@gmail.com>
2025-02-13 10:15:09 -0300

    Feat: Atualização de Pacotes

---
+++ BackEnd/Logs/UserLogs.js
@@ -0,0 +1,21 @@
+//LOGS DE SERVIÇOS
+import User from "../Models/userModel.js";
+import connected from "../Database/Database.js";
+import chalk from "chalk";
+
+
+connected.sync({})
+    .then(() => {
+        return User.findAll(); 
+    })
+    .then(users => {
+        console.log(chalk.blue("TODOS OS DADOS:", users.map(user => user.toJSON())));
+        console.log(chalk.yellow("TABELA REFERENTE:", User.getTableName()));
+        console.log("QUANTIDADE DE DADOS:", User.count(), '\n');
+        console.log(chalk.white.bgWhite("------------------------------------------\n"));
+    })
+    .catch(error => {
+        console.error("Erro ao sincronizar a tabela", error);
+    });
+
+    export default User;
