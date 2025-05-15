011c710668a4eb987edfb359e29f38613219ea01
IsraelBryan <umbreongame0@gmail.com>
2025-05-15 10:02:33 -0300

    Feat: Fix Bugs

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const port = 5171;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // O endereço do seu frontend (ajuste conforme necessário)
+    origin: "http://zelloapp.com.br", // O endereço do seu frontend (ajuste conforme necessário)
     credentials: true, // Permite o envio de cookies, como o token JWT
   })
 );
